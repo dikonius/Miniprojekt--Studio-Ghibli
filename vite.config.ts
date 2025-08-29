a1c1cb4541b3043dba7a44124e9bfbbed525a43c
dikonius <eazy777er@gmail.com>
2025-08-29 12:03:26 +0200

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Miniprojekt-1/'
+  base: '/Miniprojekt--Studio-Ghibli/'
 })
